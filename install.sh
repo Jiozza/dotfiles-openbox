cf7b6e496863f4bc2aaeee89adfdf92b5809e210
stojshic <stojshic@arch>
2022-01-24 00:42:23 +0100

    Trying to fix a bug

--- install.sh
+++ install.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
-source /root/ArchTitus/setup.conf
 
-grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERNAME}|g" @
+grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERN}|g" @
 
 pushd dotfiles-openbox/ && \
-  bash -c 'cp -v -r {.*,*} /home/${USERNAME}/' && \
+  bash -c 'cp -v -r {.*,*} /home/${USERN}/' && \
 popd
 
-rm /home/${USERNAME}/README.md && rm /home/${USERNAME}/LICENSE && rm -rf /home/${USERNAME}/.git
+rm /home/${USERN}/README.md && rm /home/${USERN}/LICENSE && rm -rf /home/${USERN}/.git
 
-cd /home/${USERNAME}/.icons/
+cd /home/${USERN}/.icons/
 tar -Jxvf oomox-aesthetic-light.tar.xz && tar -Jxvf oomox-aesthetic-dark.tar.xz
 sudo cp -r {oomox-aesthetic-light,oomox-aesthetic-dark} /usr/share/icons/
-rm -r /home/${USERNAME}/.icons/{oomox-aesthetic-light,oomox-aesthetic-dark,*.tar.xz} # Delete unnecessary files
+rm -r /home/${USERN}/.icons/{oomox-aesthetic-light,oomox-aesthetic-dark,*.tar.xz} # Delete unnecessary files
 
 fc-cache -rv
 
-sudo gpasswd -a ${USERNAME} video
+sudo gpasswd -a ${USERN} video
