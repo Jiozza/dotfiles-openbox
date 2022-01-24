6e8bc1d8edd73aa999af4d727c883a37b117d747
stojshic <stojshic@arch>
2022-01-24 04:33:44 +0100

    Auto remove script install files

--- install.sh
+++ install.sh
@@ -5,14 +5,17 @@ grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERNAME}|g" @
 
 cp -v -r dotfiles-openbox/. /home/${USERNAME}
 
-rm /home/${USERNAME}/README.md && rm /home/${USERNAME}/LICENSE && rm -rf /home/${USERNAME}/.git
-
 cd /home/${USERNAME}/.icons/
 tar -Jxvf oomox-aesthetic-light.tar.xz && tar -Jxvf oomox-aesthetic-dark.tar.xz
 rm -r *.xz
 mv * /usr/share/icons/
 
 fc-cache -rv
+
+# Clean up the mess
+cd /home/${USERNAME}
+rm -rf README.md LICENSE .git install.sh
+rm -rf /dotfiles-openbox
 
 sudo gpasswd -a ${USERNAME} video
 sudo chown -R ${USERNAME}:${USERNAME} /home/$USERNAME/.
