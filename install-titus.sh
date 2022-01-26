0e617be840ba5d82af871bb82ce1caee6e71d5e2
stojshic <stojshic@arch>
2022-01-26 18:57:51 +0100

    Revert "Added option to choose between minimal/full installation"
    This reverts commit 27ef8862c710acef6222527287c5ebbafdf13410.

--- install-titus.sh
+++ install-titus.sh
@@ -5,14 +5,12 @@ grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERNAME}|g" @
 
 cp -v -r dotfiles-openbox/. ~/
 
-cd .icons
-cat oomox* | tar -Jxvf - -i
-rm -r /*.xz
-sudo mv /oomox-aesthetic* /usr/share/icons/
+tar -Jxvf .icons/oomox-aesthetic*
+rm -r .icons/*.xz
+sudo mv .icons/oomox-aesthetic* /usr/share/icons/
 
 fc-cache -rv
 
-cd ~
 # Clean up the mess
 rm -rf README.md LICENSE .git install.sh install-titus.sh dotfiles-openbox
 
--- install.sh
+++ install.sh
@@ -5,13 +5,12 @@ grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USER}|g" @
 
 cp -v -r dotfiles-openbox/. ~
 
-cat oomox* | tar -Jxvf - -i
+tar -Jxvf ./icons/oomox-aesthetic*
 rm -r *.xz
 sudo mv oomox-aesthetic* /usr/share/icons/
 
 fc-cache -rv
 
-cd ~
 # Clean up the mess
 rm -rf README.md LICENSE .git install.sh install-titus.sh dotfiles-openbox
 
