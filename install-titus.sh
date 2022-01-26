e9f64a86e952fee562f86ec8a47dc1610fed9b54
stojshic <stojshic@arch>
2022-01-26 15:38:58 +0100

    Added two different installers

---
+++ install-titus.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+source ~/ArchTitus/setup.conf
+
+cd ~
+grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERNAME}|g" @
+
+cp -v -r dotfiles-openbox/. ~/
+
+tar -Jxvf .icons/oomox-aesthetic*
+rm -r .icons/*.xz
+sudo mv .icons/. /usr/share/icons/
+
+fc-cache -rv
+
+# Clean up the mess
+rm -rf README.md LICENSE .git install.sh install-titus.sh dotfiles-openbox
+
+sudo gpasswd -a ${USERNAME} video
--- install.sh
+++ install.sh
@@ -1,21 +1,18 @@
 #!/bin/bash
-source /root/ArchTitus/setup.conf
 
 grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USERNAME}|g" @
 
-cp -v -r dotfiles-openbox/. /home/${USERNAME}
+cp -v -r dotfiles-openbox/. /home/${USER}
 
-cd /home/${USERNAME}/.icons/
+cd /home/${USER}/.icons/
 tar -Jxvf oomox-aesthetic-light.tar.xz && tar -Jxvf oomox-aesthetic-dark.tar.xz
 rm -r *.xz
-mv * /usr/share/icons/
+sudo mv * /usr/share/icons/
 
 fc-cache -rv
 
 # Clean up the mess
-cd /home/${USERNAME}
-rm -rf README.md LICENSE .git install.sh
-rm -rf /dotfiles-openbox
+cd /home/${USER}
+rm -rf README.md LICENSE .git install.sh install-titus.sh dotfiles-openbox
 
-sudo gpasswd -a ${USERNAME} video
-sudo chown -R ${USERNAME}:${USERNAME} /home/$USERNAME/.
+sudo gpasswd -a ${USER} video
