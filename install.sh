27ef8862c710acef6222527287c5ebbafdf13410
stojshic <stojshic@arch>
2022-01-26 18:57:39 +0100

    Added option to choose between minimal/full installation

--- install.sh
+++ install.sh
@@ -5,12 +5,13 @@ grep -rl 'iruha' dotfiles-openbox/ | xargs -i@ sed -i "s|iruha|${USER}|g" @
 
 cp -v -r dotfiles-openbox/. ~
 
-tar -Jxvf ./icons/oomox-aesthetic*
+cat oomox* | tar -Jxvf - -i
 rm -r *.xz
 sudo mv oomox-aesthetic* /usr/share/icons/
 
 fc-cache -rv
 
+cd ~
 # Clean up the mess
 rm -rf README.md LICENSE .git install.sh install-titus.sh dotfiles-openbox
 
